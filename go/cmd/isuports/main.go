fc91e1678dc4245b7dad107f76c89a7571b7cb78
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2022-07-23 17:51:02 +0900

    log off

--- go/cmd/isuports/main.go
+++ go/cmd/isuports/main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	isuports "github.com/isucon/isucon12-qualify/webapp/go"
-
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"runtime"
 )
 
 func main() {
-	runtime.SetBlockProfileRate(1)
-	runtime.SetMutexProfileFraction(1)
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
 	isuports.Run()
 }
