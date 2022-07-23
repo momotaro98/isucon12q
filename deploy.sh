1f73f1294f541e08b5c41f96dedeecb8c399b7ff
Tomomichi <nogut0123@users.noreply.github.com>
2022-07-23 13:49:33 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,8 @@ sudo systemctl daemon-reload
 echo 'Updating config file...'
 sudo cp "$DIR/nginx.conf" /etc/nginx/nginx.conf
 # sudo cp "$DIR/redis.conf" /etc/redis/redis.conf
-sudo cp "$DIR/my.cnf" /etc/mysql/conf.d/my.cnf
+sudo cp "$DIR/mysqld.cnf" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp "$DIR/10_schema.sql" /home/isucon/webapp/sql/tenant/10_schema.sql
 echo 'Updated config file!'
 
 echo 'Restarting services...'
